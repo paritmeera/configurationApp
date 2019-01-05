e2188a668818a7ede215b758f122800909d27e49
paritmeera <meeraparit@gmail.com>
2019-01-05 17:47:37 +0530

    second change

--- config-server/src/main/java/com/cluster/config/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/cluster/config/configserver/ConfigServerApplication.java
@@ -3,7 +3,9 @@ package com.cluster.config.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
